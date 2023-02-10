a8478a10f758c38ae134bf9f40e65cf2eac4b26b
Varchus <varchushebbar01@gmail.com>
2023-02-10 17:58:27 +0530

    update the server url

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -29,7 +29,7 @@ const RenderCards =({ data,title }) => {
           setLoading(true);
 
           try{
-               const response= await fetch('http://localhost:8080/api/v1/post', {
+               const response= await fetch('https://bob-2-0.onrender.com/api/v1/post', {
                     method:'GET',
                     headers: {
                          'Content-Type': 'application/json',
